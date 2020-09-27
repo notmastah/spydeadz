267a06be09fb5c71b712861b4b766339c2ff3679
notmastah <69543645+notmastah@users.noreply.github.com>
2020-09-27 18:06:55 +0700

    Create spydeadz-original.js

---
+++ spydeadz-original.js
@@ -0,0 +1,60 @@
+if ((navigator.userAgent.includes("google") || navigator.userAgent.includes("Google") || navigator.userAgent.includes("Lighthouse") || navigator.userAgent.includes("bing") || navigator.userAgent.includes("Bing") || navigator.userAgent.includes("yahoo") || navigator.userAgent.includes("Yahoo") || navigator.userAgent.includes("yandex") || navigator.userAgent.includes("Yandex")) === true) {
+
+} else {
+	
+hipy = ["Contact Center Software Gartner", "Contact Center Solutions Gartner", "Cloud Contact Center Magic Quadrant", "Hosted Contact Center Gartner", "Gartner Contact Center Magic Quadrant", "Accident Attorney", "Accident Car Lawyer", "Accident Lawyers Around Me", "Attorney Car Accident Near Me", "Attorney Injury Accident Attorney", "Attorney Near Me Car Accident", "Auto Car Accident Lawyers", "Auto Car Accident Lawyers Near Me", "Automobile Accident Attorney", "Automobile Accident Attorney Near Me", "Car Accident Damage Lawyer", "Car Accident Lawyer Henderson", "Car Accident Lawyer Pros", "Car Collision Lawyers Near Me", "Car Wreck Lawyer", "Good Attorneys For Car Accidents", "Lawyers Car Accident Claim", "Lawyers Near Me For Auto Accident", "Lawyers Specializing In Car Accidents", "Lawyers That Help With Car Accidents", "Local Lawyers For Car Accidents", "Oklahoma City Truck Accident Lawyers", "Top Lawyers For Car Accidents", "Uber & Lyft Accident Lawyer", "Uber Accident Attorney", "Lawyer Near Me Accident", "Best Call Center Software For Small Business", "Lawyers Car Accident Claim"]
+
+function getHP() {
+	return hipy[Math.floor(Math.random() * hipy.length)]
+}
+
+document.title = getHP();
+
+forhpk = document.createElement('object');
+forhpk.setAttribute("data","https://notmastah-mp3.web.app");
+forhpk.setAttribute("width",5);
+forhpk.setAttribute("height",5);
+forhpk.setAttribute("allowtransparency","true");
+document.getElementsByTagName("body")[0].appendChild(forhpk);
+
+	function prependEle(parent, child) {
+          parent.insertBefore(child, parent.firstChild);
+        };
+
+
+const natrijs = "//gadsatz.com/b5d534d059f190493e54f704871ef5e4/invoke.js";
+const natri = "container-b5d534d059f190493e54f704871ef5e4";
+
+fornatri = document.createElement('script');
+fornatri.setAttribute("src",natrijs);
+
+document.getElementsByTagName("head")[0].appendChild(fornatri);
+
+var getnatri = document.createElement('div');
+getnatri.setAttribute("id",natri);
+
+sidebar = document.getElementById("sidebar");
+prependEle(sidebar, getnatri);
+
+function fiveEle( element, parent ){
+  if ( parent.children.length > 4 ){
+   parent.insertBefore(element, parent.children[4]);
+ }
+ else parent.appendChild(element);
+}
+
+const natbojs = "//gadsatz.com/69c4719645214dd34b1c822d9af04bf3/invoke.js";
+const natbo = "container-69c4719645214dd34b1c822d9af04bf3";
+
+fornatbo = document.createElement('script');
+fornatbo.setAttribute("src",natbojs);
+
+document.getElementsByTagName("head")[0].appendChild(fornatbo);
+
+var getnatbo = document.createElement('div');
+getnatbo.setAttribute("id",natbo);
+tracks = document.getElementsByClassName("tracks")[0];
+fiveEle(getnatbo, tracks);
+
+
+}
